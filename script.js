21d87a545ba61b0c690507d1b48f7db7f84464d3
joanna-laurel <191937222+joanna-laurel@users.noreply.github.com>
2025-02-24 10:26:56 -0500

    Comment code suggestions and fix scoring glitch
    Resolved: scoring issue that somehow slipped in unnoticed after testing
    and before last commit and was resetting scores to 0 each round
    
    Commented: various functions with streamlined code proposals to test
    out after human choice mechanism is fixed -- aka once game is fully
    functional, let's see if code can be simplified
    
    Purged: comments containing failed functions, console.log functions no
    longer needed for testing code, obsolete notes to self, various detritus

--- script.js
+++ script.js
@@ -1,111 +1,87 @@
 //Game progression pseudocode:
 
-// Prompt user to input rock, paper, or scissors        
-// Convert input to all lowercase                                  
-// If converted input /= rock, paper, or scissors:                 
-    // Print message saying so                                      
-    // Go back to line 3 until they get it right, then proceed     
-// Generate random number                                          
-// Convert number to rock, paper, or scissors (equal chances obv)  
-// Compare human and computer selections                           
-// If it's a tie:                                                  
+//Prompt user to input rock, paper, or scissors        
+//Convert input to all lowercase                                  
+//If converted input /= rock, paper, or scissors:                 
+    //Print message saying so                                      
+    //Go back to line 3 until they get it right, then proceed     
+//Generate random number                                          
+//Convert number to rock, paper, or scissors (equal chances obv)  
+//Compare human and computer selections                           
+//If it's a tie:                                                  
     // Print tie message                                           
     // Go back to line 3
-// If there's a winner:                                            
+//If there's a winner:                                            
     // Add one point to winner's running total score               
-// If human score + computer score = 5
-    // Compare human and computer scores and determine winner      
-    // Print message that game is over, who won, and the score
-        //Possible customized messages depending on scores
-    // Reset scores to 0
-    // Go back to line 3 (we'll assume they want to play again)
-// If total score < 5, 
-    // Print message declaring round winner and current running scores
-    // Go back to line 3                            
+//If human score + computer score = 5:
+    //Compare human and computer scores and determine winner      
+    //Print message that game is over, who won, and the score
+    //Reset scores to 0
+    //Go back to line 3 (we'll assume they want to play again)
+//If total score < 5:
+    //Print message declaring round winner and current running scores
+    //Go back to line 3                            
+
+//*******************************************************************//
 
 
-// remaining problems this session (first 4 clearly related):
+//Remaining to-do as of this commit:
 
-// game ends properly, goes to welcome message, but doesn't reset scores
-// user is welcomed to the game at the beginning of second round 
-// humanChoice so far accepts everything or nothing as valid lol
-// humanChoice prompt accepts both valid and invalid options (although if they are invalid the human loses, so there's that incentive)
-
-
-// fixed since last time:
-
-// not really a fix but I figured out the Live Preview extension so I can use prompt function
-// the scoring does actually work, at least starting with round 2 (must return values to parent function!)
-// the round counter works how I intended: ties don't count, best 3 out of 5 then game ends
-// game actually starting at the second round issue (no more practice round)
-// this also fixed the welcome message issue
-// functions not returning updated values (return the function not the value!)
-// 
+//Fix getHumanChoice
+//Test code streamlining possibilities commented next to various functions
 
 
 
 let humanScore = 0;
 let computerScore = 0;
 
-playGame();   
-    
-// const computerChoice = getComputerChoice();  //value is rock, paper, or scissors
-// const humanChoice = getHumanChoice();        //value is rock, paper, or scissors
-// const roundWinner = findRoundWinner();   //value is human, computer, or tie
+playGame(); 
+
 
 function playGame() { 
-    console.log("Welcome to a new game of Rock, Paper, Scissors");
+    console.log("Welcome to a new game of Rock, Paper, Scissors.");
     
-    if ((humanScore + computerScore) > 0) {
+    if ((humanScore + computerScore) > 0) { //operates as reset for new games
         humanScore = 0; computerScore = 0
     }
 
-    while ((humanScore + computerScore) < 5) {
-
-        
-        console.log("Total scores: " + (humanScore + computerScore)); //at this point the scores work
+    while ((humanScore + computerScore) < 5) { //we aren't counting tie rounds!
+        //getComputerChoice(); 
+        //getHumanChoice(); 
+        //findRoundWinner();
         playRound(); 
-        console.log("Roundwinner is " + roundWinner);
-        console.log("the round is played and now we need scores"); //first round doesn't tally
+        console.log(roundWinner)
         getHumanScore();
         getComputerScore();
-        console.log("The score is Human: " + humanScore + ", Computer: " + computerScore)
-        // let totalScore = (humanScore + computerScore);
-        // if (totalScore === 5) {
-        //     gameOver;
-        // } else {
-        //     console.log("The current score is " + humanScore, computerScore);   
-        } gameOver();
-   
+        console.log("The score is Human: " + humanScore + ", Computer: " + computerScore) 
+    } 
+    
+    gameOver();
 }
     
-function playRound() {
-    // const computerChoice = getComputerChoice();  //value is rock, paper, or scissors
-    // const humanChoice = getHumanChoice();        //value is rock, paper, or scissors
-    // const roundWinner = findRoundWinner();   //value is human, computer, or tie
-    // getComputerChoice(); //working the first time
-    // getHumanChoice(); //half working -- need to fix null situation
-    // findRoundWinner(); //now returns value properly to playGame
-    // return (computerChoice), (humanChoice), (roundWinner);
-    return getComputerChoice(), getHumanChoice(), findRoundWinner(); //returning scores now every 3 rounds????
+function playRound() { //does this need to even be its own thing?
+    return getComputerChoice(), getHumanChoice(), findRoundWinner(); 
 }    
 
-function getComputerChoice(computerChoice) {               
+function getComputerChoice() { //too wordy? streamline?        
     let choice = ((Math.random() * 9) + 1);  //all #s 1.0000000-9.9999999
     console.log("The computer has made a choice and awaits the human's decision.");
     if (choice < 4) {
-        computerChoice = "rock";             //between 1 and 4
+        //return "rock";
+        computerChoice = "rock"; //all #s between 1 and 4
         return computerChoice; 
     } else if (choice > 7) {
-        computerChoice = "scissors";         //between 7 and basically-10
+        //return "scissors";
+        computerChoice = "scissors"; //all #s between 7 and basically-10
         return computerChoice;
     } else {
-        computerChoice = "paper";            //between 4 and 7 
-        return computerChoice;  
+        //return "paper";
+        computerChoice = "paper"; //all #s between 4 and 7 
+        return computerChoice; 
     }              
 }  
         
-function getHumanChoice(humanChoice) {  //currently works for no choices         
+function getHumanChoice() {  //fix this mess!        
     let choice = prompt("Please enter rock, paper, or scissors.").toLowerCase();   
     // if (humanChoice === "paper") {
     // // ((humanChoice === "paper") || (humanChoice === "rock") || (humanChoice === "scissors")) {
@@ -118,11 +94,11 @@ function getHumanChoice(humanChoice) {  //currently works for no choices
         console.log("I'm sorry, that is not on the list of approved items"); 
         // return ("paper");
         humanChoice = choice 
-        return (humanChoice);  
+        return humanChoice;  
     // }  
 }    
  
-function findRoundWinner(roundWinner) {              //returns human, computer, or tie
+function findRoundWinner() { //merge this with getHuman/ComputerScore???
     console.log("The human has chosen " + humanChoice + ".");
     console.log("what about the computer");
     console.log("The computer has chosen " + computerChoice + "."); 
@@ -130,47 +106,49 @@ function findRoundWinner(roundWinner) {              //returns human, computer, 
         ((humanChoice === "scissors") && (computerChoice === "paper")) ||
         ((humanChoice === "paper") && (computerChoice === "rock"))) {                      
             console.log("The human is victorious and has gained one point."); 
-            // return ("human");
+            //return ("human");
+            //return humanScore++, computerScore;
             roundWinner = ("human");
             return roundWinner;
         } else if (humanChoice === computerChoice) { 
             console.log("It's a tie!"); 
-            // return ("tie");  
-            roundWinner = ("tie");
-            return roundWinner;
+            //return ("tie");
+            //return humanScore,computerScore;
+            roundWinner = ("tie");  //is it necessary to return anything for a tie?
+            return roundWinner;   
         } else {
             console.log("The computer triumphs!!"); 
-            // return ("computer");  
+            //return ("computer");  
+            //return humanScore, computerscore++;
             roundWinner = ("computer");
             return roundWinner;    
         }     
 }   
-
-console.log("random interjection")
        
-function getHumanScore(humanScore) {  
-    console.log(humanScore);
-    console.log(roundWinner);
-    // let pointForHuman = humanScore++;
+function getHumanScore() {  
     if (roundWinner === "human") {
+        console.log(humanScore);
+        console.log("human is roundwinner");
         humanScore++;
+        console.log(humanScore)
     } return humanScore;   
 }
 
-function getComputerScore(computerScore) {
-    console.log(computerScore);
-    console.log(roundWinner);
+function getComputerScore() {
     if (roundWinner === "computer") {
+        console.log(computerScore); //undefined
+        console.log("computer is roundwinner");
         computerScore++;
+        console.log(computerScore) //NaN
     } return computerScore;
 }
 
 function gameOver() { 
-   let humanWins = (humanScore > computerScore);
+    //if (humanScore > computerScore)
+   let humanWins = (humanScore > computerScore); 
    if (humanWins === true) {
     console.log("The lucky human has won with " + humanScore + " points.")
     console.log("Let's play again!");
-
     playGame();
    } else {
     console.log("Alas, the computer prevails. The human loses " + humanScore + " to " + computerScore + ".");
@@ -179,17 +157,6 @@ function gameOver() {
    }
 }
 
-
-// function getHumanChoice(humanChoice) {
-//     issues prompt("Rock, paper, or scissors?")
-//     converts to lowercase = (humanChoice.toLowerCase())
-//     if (humanChoice.toLowerCase() === "rock" || "scissors" || "paper") {
-//         return (humanChoice.toLowerCase);
-//     } else {
-//         prompt("Try again!");
-
-
- 
 
 // ALTERNATIVE playRound() idea with 7 different message possibilities:
 // 
